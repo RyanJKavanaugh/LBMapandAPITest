03cd36a70afa390a4444c8dc0d36fd262cdf0fb5
Ryan Kavanaugh <ryan.kavanaugh@crc-corp.com>
2017-10-23 16:02:54 -0700

    Test With New Functions

--- LBMapWFunc.py
+++ LBMapWFunc.py
@@ -22,28 +22,22 @@ from pyvirtualdisplay import Display
 # display.start()
 
 # Function to access Agency LB Website and retrieve Google Maps API link
-def Get_Google_Maps_API_Link(driver, url):
+def get_Google_Maps_API_Link(driver, url):
     # Access web page
     driver.get(url)
     # Gather all elements that have an img tag
     _inputs = driver.find_elements_by_xpath('//img')
-    # Print page title for easy reference and review
+    # print page title for easy reference and review
     print driver.title
     # Loop through all img web elements and find element holding the Google API Link
     for input in _inputs:
         item = str(input.get_attribute('src'))
         if 'https://maps.googleapis.com/maps/api' in item:
             # Return the Google API link
-            return input.get_attribute('src')
-
-
-# Function to get the status code for an API request, given the proper link
-def Get_Status_code(apiLink):
+            apiLink = input.get_attribute('src')
     apiRequest = requests.get(apiLink)
     statusCode = apiRequest.status_code
-    print statusCode
     return statusCode
-
 
 class Verify_LB_Web_Map(unittest.TestCase):
 
@@ -52,81 +46,77 @@ class Verify_LB_Web_Map(unittest.TestCase):
         driver = webdriver.Chrome()
         testcounter = 0
 
-        # Idaho LB Web Test ( With comments that apply for each following agency)
+        # Idaho LB Web Test
         urlID = 'http://lb.511.idaho.gov/idlb/'
         # Get Map link that connects with Google API
-        apiLink = Get_Google_Maps_API_Link(driver, urlID)
-        # Get the status of the request to the Google API
-        statuscodeID = Get_Status_code(apiLink)
+        IDStatusCode = get_Google_Maps_API_Link(driver, urlID)
+        print IDStatusCode
 
         # If the map is down, then the test will fail and trigger an email in Jenkins
-        if  statuscodeID != 200:
+        if  IDStatusCode != 200:
             print 'LB Idaho Map Is Down'
             testcounter += 1
 
-
         # Louisiana
         urlLA = 'https://lb.511la.org/lalb/'
-        linkLA = Get_Google_Maps_API_Link(driver, urlLA)
-        statuscodeLA = Get_Status_code(linkLA)
+        LAStatusCode = get_Google_Maps_API_Link(driver, urlLA)
+        print LAStatusCode
 
-        if  statuscodeLA != 200:
+        if  LAStatusCode != 200:
             print 'LB Loisiana Map Is Down'
             testcounter += 1
 
-
         # Nebraska
         urlNE = 'https://lb.511.nebraska.gov/nelb/'
-        linkNE = Get_Google_Maps_API_Link(driver, urlNE)
-        statuscodeNE = Get_Status_code(linkNE)
+        linkNE = get_Google_Maps_API_Link(driver, urlNE)
+        rNE = requests.get(linkNE)
+        print rNE.status_code
 
-        if  statuscodeNE != 200:
+        if  rNE.status_code != 200:
             print 'LB Nebraska Map Is Down'
             testcounter += 1
 
-
         # Iowa
         urlIA = 'https://lb.511ia.org/ialb/'
-        linkIA = Get_Google_Maps_API_Link(driver, urlIA)
-        statuscodeIA = Get_Status_code(linkIA)
+        linkIA = get_Google_Maps_API_Link(driver, urlIA)
+        rIA = requests.get(linkIA)
+        print rIA.status_code
 
-        if  statuscodeIA != 200:
+        if  rIA.status_code != 200:
             print 'LB Iowa Map Is Down'
             testcounter += 1
 
-
         # Sacog
         urlSACOG = 'http://sa.carsstage.org/salbweb/'
-        linkSACOG = Get_Google_Maps_API_Link(driver, urlSACOG)
-        statuscodeSACOG = Get_Status_code(linkSACOG)
+        linkSACOG = get_Google_Maps_API_Link(driver, urlSACOG)
+        rSACOG = requests.get(linkSACOG)
+        print rSACOG.status_code
 
-        if  statuscodeSACOG != 200:
+        if  rSACOG.status_code != 200:
             print 'LB Sacramento Map Is Down'
             testcounter += 1
 
-
         # Sandag
         urlSAN = 'https://lbw.511sd.com/lbweb/'
-        linkSAN = Get_Google_Maps_API_Link(driver, urlSAN)
-        statuscodeSAN = Get_Status_code(linkSAN)
+        linkSAN = get_Google_Maps_API_Link(driver, urlSAN)
+        rSAN = requests.get(linkSAN)
+        print rSAN.status_code
 
-        if  statuscodeSAN != 200:
+        if  rSAN.status_code != 200:
             print 'LB San Fransisco Map Is Down'
             testcounter += 1
 
-
         # Minnesota
         urlMN = 'https://lb.511mn.org/mnlb/'
-        linkMN = Get_Google_Maps_API_Link(driver, urlMN)
-        statuscodeMN = Get_Status_code(linkMN)
+        linkMN = get_Google_Maps_API_Link(driver, urlMN)
+        rMN = requests.get(linkMN)
+        print rMN.status_code
 
-        if  statuscodeMN != 200:
+        if  rSAN.status_code != 200:
             print 'LB Minnesota Map Is Down'
             testcounter += 1
 
-
         driver.quit()
-
 
         if testcounter > 0:
             assert False
