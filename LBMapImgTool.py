01b557ab75ec82d1dc735b59c8044de27a9c2155
Ryan Kavanaugh <ryan.kavanaugh@crc-corp.com>
2017-10-23 10:14:55 -0700

    Update With //img Tool

--- LBMapImgTool.py
+++ LBMapImgTool.py
@@ -32,14 +32,13 @@ class Verify_Idaho_Links(unittest.TestCase):
         urlID = 'http://crc-prod-id-wf-elb-382957924.us-west-2.elb.amazonaws.com/idlb/'
         driver.get(urlID)
 
-        _inputs = driver.find_elements_by_xpath('//img')
-        for input in _inputs:
+        _inputsID = driver.find_elements_by_xpath('//img')
+        for input in _inputsID:
             item = str(input.get_attribute('src'))
             if 'https://maps.googleapis.com/maps/api' in item:
                  print input.get_attribute('src')
                  linkID = input.get_attribute('src')
-        #mapIdaho = driver.find_element_by_xpath("//*[@id='j_idt141']/img")
-        #linkID = mapIdaho.get_attribute('src')
+
         rID = requests.get(linkID)
         print rID.status_code
 
@@ -47,16 +46,17 @@ class Verify_Idaho_Links(unittest.TestCase):
             print 'LB Idaho Map Is Down'
           #  testcounter += 1
 
+
         # Louisiana
         urlLA = 'https://lb.511la.org/lalb/'
         driver.get(urlLA)
-        time.sleep(1)
-        mapLA = driver.find_element_by_xpath('//*[@id="j_idt155"]/img')
-        linkLA = mapLA.get_attribute('src')
+        inputsLA = driver.find_elements_by_xpath('//img')
+        for input in inputsLA:
+            item = str(input.get_attribute('src'))
+            if 'https://maps.googleapis.com/maps/api' in item:
+                 print input.get_attribute('src')
+                 linkLA = input.get_attribute('src')
 
-        # test = driver.find_element_by_xpath("//*[text()[contains(.,'mapPanelContent')]]")
-        # print test
-        # "//*[contains(text(), 'Delete this route')]"
         rLA = requests.get(linkLA)
         print rLA.status_code
 
@@ -64,11 +64,16 @@ class Verify_Idaho_Links(unittest.TestCase):
             print 'LB Loisiana Map Is Down'
             testcounter += 1
 
+
         # Nebraska
         urlNE = 'https://lb.511.nebraska.gov/nelb/'
         driver.get(urlNE)
-        mapNE = driver.find_element_by_xpath('//*[@id="j_idt346"]/img')
-        linkNE = mapNE.get_attribute('src')
+        inputsNE = driver.find_elements_by_xpath('//img')
+        for input in inputsNE:
+            item = str(input.get_attribute('src'))
+            if 'https://maps.googleapis.com/maps/api' in item:
+                 print input.get_attribute('src')
+                 linkNE = input.get_attribute('src')
 
         rNE = requests.get(linkNE)
         print rNE.status_code
@@ -77,11 +82,16 @@ class Verify_Idaho_Links(unittest.TestCase):
             print 'LB Nebraska Map Is Down'
             testcounter += 1
 
+
         # Iowa
         urlIA = 'https://lb.511ia.org/ialb/'
         driver.get(urlIA)
-        mapIA = driver.find_element_by_xpath('//*[@id="j_idt383"]/img')
-        linkIA = mapIA.get_attribute('src')
+        inputsIA = driver.find_elements_by_xpath('//img')
+        for input in inputsIA:
+            item = str(input.get_attribute('src'))
+            if 'https://maps.googleapis.com/maps/api' in item:
+                 print input.get_attribute('src')
+                 linkIA = input.get_attribute('src')
 
         rIA = requests.get(linkIA)
         print rIA.status_code
@@ -90,12 +100,16 @@ class Verify_Idaho_Links(unittest.TestCase):
             print 'LB Iowa Map Is Down'
             testcounter += 1
 
+
         # Sacog
         urlSACOG = 'http://sa.carsstage.org/salbweb/'
         driver.get(urlSACOG)
-        mapSACOG = driver.find_element_by_xpath('//*[@id="j_idt122"]/img')
-        linkSACOG = mapSACOG.get_attribute('src')
-
+        inputsSACOG = driver.find_elements_by_xpath('//img')
+        for input in inputsSACOG:
+            item = str(input.get_attribute('src'))
+            if 'https://maps.googleapis.com/maps/api' in item:
+                print input.get_attribute('src')
+                linkSACOG = input.get_attribute('src')
         rSACOG = requests.get(linkSACOG)
         print rSACOG.status_code
 
@@ -103,12 +117,16 @@ class Verify_Idaho_Links(unittest.TestCase):
             print 'LB Sacramento Map Is Down'
             testcounter += 1
 
+
         # Sandag
         urlSAN = 'https://lbw.511sd.com/lbweb/'
         driver.get(urlSAN)
-        mapSAN = driver.find_element_by_xpath('//*[@id="j_idt150"]/img')
-        linkSAN = mapSAN.get_attribute('src')
-
+        inputsSAN = driver.find_elements_by_xpath('//img')
+        for input in inputsSAN:
+            item = str(input.get_attribute('src'))
+            if 'https://maps.googleapis.com/maps/api' in item:
+                print input.get_attribute('src')
+                linkSAN = input.get_attribute('src')
         rSAN = requests.get(linkSAN)
         print rSAN.status_code
 
@@ -116,22 +134,16 @@ class Verify_Idaho_Links(unittest.TestCase):
             print 'LB San Fransisco Map Is Down'
             testcounter += 1
 
+
         # Minnesota
         urlMN = 'https://lb.511mn.org/mnlb/'
         driver.get(urlMN)
-        print driver.title
-        #imageWait = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//*[@id='j_idt369']/img")))
-        try:
-            mapMN = driver.find_element_by_xpath('//*[@id="j_idt166"]/img')
-        except:
-            try:
-                mapMN = driver.find_element_by_xpath('//*[@id="j_idt368"]/img')
-            except:
-                try:
-                    mapMN = driver.find_element_by_xpath('//*[@id="j_idt365"]/img')
-                except:
-                    pass
-        linkMN = mapMN.get_attribute('src')
+        inputsMN = driver.find_elements_by_xpath('//img')
+        for input in inputsMN:
+            item = str(input.get_attribute('src'))
+            if 'https://maps.googleapis.com/maps/api' in item:
+                print input.get_attribute('src')
+                linkMN = input.get_attribute('src')
 
         rMN = requests.get(linkMN)
         print rMN.status_code
